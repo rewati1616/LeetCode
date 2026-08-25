b458972e7f4dedb0a554c0c3b6b17805ba4c817d
Rewati Warhade <159109059+rewati1616@users.noreply.github.com>
2026-08-25 21:51:55 +0530

    Fix formatting and syntax in longest substring solution

--- 3LongestSubstringWithoutRepeatingCharacters.cpp
+++ 3LongestSubstringWithoutRepeatingCharacters.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         unordered_set<char> st;
-        int left = 0;
+        int left = 0;  
         int maxLen = 0;
 
         for(int right = 0; right < s.size(); right++) {
             
-            while(st.count(s[right])) {
+            while(st.count(s[right])) {   
                 st.erase(s[left]);
                 left++;
             }
 
-            st.insert(s[right]);
+            st.insert(s[right]);   
             maxLen = max(maxLen, right - left + 1);
         }
-
+   
         return maxLen;
     }
 };
