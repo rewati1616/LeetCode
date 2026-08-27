55331201e6ff988753c5dae24d8f99018000eff4
Rewati Warhade <159109059+rewati1616@users.noreply.github.com>
2026-08-27 22:49:09 +0530

    Implement longest palindromic substring solution

---
+++ 5LongestPalindromicSubstring.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    int expand(string &s, int left, int right){
+        while(left >= 0 && right < s.size() && s[left] == s[right]){
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+    string longestPalindrome(string s) {
+        
+        int start = 0;
+        int end = 0;
+
+        for(int i = 0; i < s.size(); i++){
+
+            int len1 = expand(s, i, i);       // odd length
+            int len2 = expand(s, i, i+1);     // even length
+
+            int len = max(len1, len2);
+
+            if(len > end - start){
+                start = i - (len-1)/2;
+                end = i + len/2;
+            }
+        }
+
+        return s.substr(start, end-start+1);
+    }
+};
