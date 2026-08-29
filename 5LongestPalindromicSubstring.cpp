ec1b588051ba690b5881fbf7960224d5a5279aa6
Rewati Warhade <159109059+rewati1616@users.noreply.github.com>
2026-08-29 23:03:17 +0530

    Refactor expand function and variable initialization

--- 5LongestPalindromicSubstring.cpp
+++ 5LongestPalindromicSubstring.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
 
     int expand(string &s, int left, int right){
-        while(left >= 0 && right < s.size() && s[left] == s[right]){
-            left--;
-            right++;
-        }
-        return right - left - 1;
+        while(left >= 0 && right < s.size() && s[left] == s[right]){ 
+            left--; 
+            right++; 
+        }  
+        return right - left - 1; 
     }
 
     string longestPalindrome(string s) {
         
-        int start = 0;
-        int end = 0;
+        int start = 0; 
+        int end = 0; 
 
-        for(int i = 0; i < s.size(); i++){
+        for(int i = 0; i < s.size(); i++){ 
 
             int len1 = expand(s, i, i);       // odd length
             int len2 = expand(s, i, i+1);     // even length
