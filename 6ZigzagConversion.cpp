2e9eb79c5160ec5a3afae6e743ec33b4907a1253
Rewati Warhade <159109059+rewati1616@users.noreply.github.com>
2026-08-29 23:01:44 +0530

    Refactor convert method in ZigzagConversion

--- 6ZigzagConversion.cpp
+++ 6ZigzagConversion.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    string convert(string s, int numRows) {
-        if (numRows == 1 || s.size() <= numRows) 
+    string convert(string s, int numRows) { 
+        if (numRows == 1 || s.size() <= numRows ) 
             return s;
 
         vector<string> rows(numRows);
