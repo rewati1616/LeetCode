4ab714d2de92d182cc68472506bc01853f617f32
Rewati Warhade <159109059+rewati1616@users.noreply.github.com>
2026-08-29 23:04:01 +0530

    Refactor longestPalindrome function for clarity

--- 5LongestPalindromicSubstring.cpp
+++ 5LongestPalindromicSubstring.cpp
@@ -11,22 +11,22 @@ public:
 
     string longestPalindrome(string s) {
         
-        int start = 0; 
-        int end = 0; 
+        int start = 0;  
+        int end = 0;  
 
         for(int i = 0; i < s.size(); i++){ 
 
             int len1 = expand(s, i, i);       // odd length
             int len2 = expand(s, i, i+1);     // even length
 
-            int len = max(len1, len2);
+            int len = max(len1, len2); 
 
-            if(len > end - start){
-                start = i - (len-1)/2;
-                end = i + len/2;
+            if(len > end - start){ 
+                start = i - (len-1)/2;  
+                end = i + len/2; 
             }
         }
 
-        return s.substr(start, end-start+1);
+        return s.substr(start, end-start+1);  
     }
 };
